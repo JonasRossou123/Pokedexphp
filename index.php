a5ada0684f3e334f3bc393b125be572f418fa53b
JonasRossou123 <jonas.rossou@hotmail.com>
2021-05-17 13:26:05 +0200

    form done,  link with api

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+$world = "hello world";
+echo "hello world!";
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<h1><?php echo $world?></h1>
+<p><?php var_dump($_GET); ?></p>
+<p><?php var_dump($_GET["id"]); ?></p>
+
+<?php
+//convert json data to string
+$api= file_get_contents ('https://pokeapi.co/api/v2/pokemon')
+echo json_encode($api)
+//convert to json string and back to array
+
+
+?>
+
+<?php
+pre_r($_POST);
+?>
+
+
+
+<p>WIE IS DEZE POKEMON?</p>
+<form class="search-poke" action="" method="POST">
+    <input type="text" name="pokemon" placeholder="Enter Pokémon">
+    <input type="text" name="id" placeholder="Enter ID">
+    <input type="submit" name="submit" value="Search">
+</form>
+</body>
+</html>
+
+<?php
+function pre_r ($array) {
+    echo'<pre>';
+    print_r($array);
+    echo '</pre>';
+}
+?>
