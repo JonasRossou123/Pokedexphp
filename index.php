f6ea560ac6deece31c8c75f291b9fb532327d52e
JonasRossou123 <jonas.rossou@hotmail.com>
2021-05-18 14:31:30 +0200

    before adding to html previous exercise

--- index.php
+++ index.php
@@ -6,25 +6,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Pokedexphp</title>
 </head>
 <body>
 
-
 <?php
 //Code won't run until something's filled in
-if (isset($_GET['pokemon'])) {
+if (isset($_POST['pokemon'])) {
 
 //pokemon added at the end of the link
-    $api = 'https://pokeapi.co/api/v2/pokemon/'.$_GET['pokemon'];
+    $api = 'https://pokeapi.co/api/v2/pokemon/' . $_POST['pokemon'];
 //link to the Pokémon it has evolved from
-    $apiEvolution = 'https://pokeapi.co/api/v2/pokemon-species/'.$_GET['pokemon'];
+    $apiEvolution = 'https://pokeapi.co/api/v2/pokemon-species/' . $_POST['pokemon'];
 
 //file_get_contents -> Reads entire file into a string
     $apistring = file_get_contents($api, true);
 //idem other link
     $apiEvolutionstring = file_get_contents($apiEvolution, true);
-
 //Takes a JSON encoded string and converts (=decode) it into a PHP variable.
     $pokemonData = json_decode($apistring, true);
 //idem other link
@@ -34,30 +32,37 @@ if (isset($_GET['pokemon'])) {
     $pokemon_id = $pokemonData['id'];
     $pokemon_image = $pokemonData['sprites']['front_default'];
     $pokemon_evolved = $pokemonEvolution['evolves_from_species']['name'];
-    $pokemon_type = '';
-
-}
-?>
-
-
-//pre_r($_POST);
+} ?>
 
 <p>WIE IS DEZE POKEMON?</p>
-<form class="search-poke" action="">
+<form class="search-poke" action="" method="POST">
     <input type="text" name="pokemon" placeholder="Enter Pokémon or ID">
-        <input type="submit" name="submit" value="Search">
+    <input type="submit" name="submit" value="Search">
 </form>
 
-
-
 <?php echo $pokemon_name ?><br>
-<?php echo $pokemon_evolved ?>
+<?php echo $pokemon_id ?><br>
+<img src='<?php echo $pokemon_image ?>' alt="Pokepicture"><br>
+<ul><?php
+    for ($x = 0; $x <= 3; $x++) { ?>
+        <li><?php echo $pokemonData['moves'][$x]['move']['name'];?></li><?php
+    }?>
+</ul>
+<?php
+if ($pokemon_evolved === null) {
+    echo 'This Pokémon has not been evolved yet';
+} else {
+    echo 'Evolved from' .$pokemon_evolved;
+    //link to the picture of the Pokémon it has evolved from
+    $apiEvolutionpicture = 'https://pokeapi.co/api/v2/pokemon/' . $pokemon_evolved;
+    $apiEvolutionpictureString = file_get_contents($apiEvolutionpicture, true);
+    $pokemonEvolutionPicture = json_decode($apiEvolutionpictureString, true);
+    $pokemon_evolved_image = $pokemonEvolutionPicture['sprites']['front_default'];
+    ?><br>
+    <img src='<?php echo $pokemon_evolved_image ?>' alt="Image of previous"><br>
+    <?php
+} ?>
 
 </body>
 </html>
 
-//function pre_r ($array) {
-    //echo'<pre>';
-    //print_r($array);
-    //echo '</pre>';
-//}
